1c4abf6564ee9d277aa02e064f9a62f25adb85a4
adithya-parambil <adithya.224416101@vcet.edu.in>
2026-02-12 17:30:53 +0530

    Add EnhancedPageCurl component implementation

---
+++ src/components/magazine/EnhancedPageCurl.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EnhancedPageCurl = () => {
+    return (
+        <div className="enhanced-page-curl">
+            {/* Enhanced page curl UI goes here */}
+            <h1>Enhanced Page Curl Component</h1>
+        </div>
+    );
+};
+
+export default EnhancedPageCurl;
